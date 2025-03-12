cc1c6dbdb16edfe48bdce859c498053ae10f799e
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-13 02:51:23 +0530

    corrected test input

--- kata.test.js
+++ kata.test.js
@@ -41,7 +41,7 @@ test('a negative number', () => {
 })
 
 test('multiple negative numbers', () => {
-	assert.throws(() => { add("-1,88,-77,\n-66\n-55") }, (err) => {
+	assert.throws(() => { add("-1,88,-77\n-66\n-55") }, (err) => {
 		assert(err instanceof Error);
 		assert.strictEqual(err.message, "negative numbers not allowed -1,-77,-66,-55")
 		return true;
