184b023d5ffe4dc19e42601eaceb51c0e13d93bf
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-13 01:39:23 +0530

    handled newlines instead of comma in numbers string

--- kata.js
+++ kata.js
@@ -8,7 +8,10 @@
 export default function add(numbers) {
 	if(numbers.length <= 1) return Number(numbers);
 
+	const NEWLINE_REGEX = /\n/g;
+
 	let sum = 0;
+	numbers = numbers.replace(NEWLINE_REGEX, ",");
 	const numbersArray = numbers.split(",");
 
 	for(let i = numbersArray.length-1; i >= 0; i--) {
