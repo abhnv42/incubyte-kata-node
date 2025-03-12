a0075b6fd8b7fcc21c831153cf8507bb3d1e3a31
Abhinav Gupta <abhnvashgpta@gmail.com>
2025-03-13 01:32:22 +0530

    added conditions to pass base case

--- kata.js
+++ kata.js
@@ -7,4 +7,13 @@
  */
 export default function add(numbers) {
 	if(numbers.length <= 1) return Number(numbers);
+
+	let sum = 0;
+	const numbersArray = numbers.split(",");
+
+	for(let i = numbersArray.length-1; i >= 0; i--) {
+		sum += Number(numbersArray[i]);
+	}
+
+	return sum;
 }
